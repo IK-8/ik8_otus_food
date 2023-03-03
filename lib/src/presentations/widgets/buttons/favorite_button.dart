c557577615f4db5e3185d0b08c0aad2ec79af869
IK8 <ik.1701@mail.ru>
2023-03-03 13:16:02 +0500

    animated heart

--- lib/src/presentations/widgets/buttons/favorite_button.dart
+++ lib/src/presentations/widgets/buttons/favorite_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 
-class FavoriteButton extends StatelessWidget {
+class FavoriteButton extends StatefulWidget {
   final ValueChanged<bool> onChanged;
   final bool value;
 
@@ -8,22 +9,45 @@ class FavoriteButton extends StatelessWidget {
       : super(key: key);
 
   @override
+  State<FavoriteButton> createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  SMIBool? _isChecked;
+
+  @override
+  void didUpdateWidget(covariant FavoriteButton oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.value != widget.value) {
+      _isChecked?.value = widget.value;
+    }
+  }
+
+  void _onRiveInit(Artboard artboard) {
+    final controller = StateMachineController.fromArtboard(artboard, 'States');
+    artboard.addController(controller!);
+
+    _isChecked = controller.findInput<bool>('IsChecked') as SMIBool;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return IconButton(
-      onPressed: () {
-        onChanged(!value);
+    return InkWell(
+      onTap: () {
+        widget.onChanged(!widget.value);
       },
-      splashColor: value ?  null : const Color(0x66e74c3c),
-      padding: EdgeInsets.zero,
-      iconSize: 30,
-      icon: value
-          ? const Icon(
-        Icons.favorite,
-        color: Color(0xffE74C3C),
-      )
-          : const Icon(
-        Icons.favorite,
-        color: Color(0xff3B3B3B),
+      child: SizedBox(
+        height: 40,
+        width: 40,
+        child: RiveAnimation.asset(
+          'assets/animation/animheartbeat.riv',
+          onInit: _onRiveInit,
+        ),
       ),
     );
   }
